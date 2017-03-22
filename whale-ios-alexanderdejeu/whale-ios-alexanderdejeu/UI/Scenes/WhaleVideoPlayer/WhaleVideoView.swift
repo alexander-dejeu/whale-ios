90695e8c2d18fbb4d8c138dc535e595b363e8ca0
Alexander Dejeu <alexdejeu@gmail.com>
2017-03-22 15:02:52 -0700

    Added profile xib

---
+++ whale-ios-alexanderdejeu/whale-ios-alexanderdejeu/UI/Scenes/WhaleVideoPlayer/WhaleVideoView.swift
@@ -0,0 +1,21 @@
+//
+//  WhaleVideoView.swift
+//  whale-ios-alexanderdejeu
+//
+//  Created by Alexander Dejeu on 3/22/17.
+//  Copyright © 2017 Do Good Technology. All rights reserved.
+//
+
+import UIKit
+
+class WhaleVideoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
